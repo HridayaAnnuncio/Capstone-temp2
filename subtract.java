a5212904db362db8fad4b6ba2ac5818374b1c13f
varuni20 <Varuni.Agarwal@st.niituniversity.in>
2019-09-28 11:28:34 +0530

    Subtract

---
+++ subtract.java
@@ -0,0 +1,15 @@
+package capstotne2;
+
+public class subtract {
+	public static int main(String[] args)
+	{
+		int subtract(int a, int b)
+		{
+			int res;
+			System.out.println("Subtract the values");
+			res= (a-b);
+			return res;
+		}
+	}
+
+}
